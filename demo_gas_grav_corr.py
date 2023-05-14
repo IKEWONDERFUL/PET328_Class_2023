59a4ba3253c921a207ebd6a1e254823f130860de
IKE WONDERFUL <129363954+IKEWONDERFUL@users.noreply.github.com>
2023-05-14 17:01:56 +0100

    demo_gas_grav_corr_upd.py

--- demo_gas_grav_corr.py
+++ demo_gas_grav_corr.py
@@ -16,7 +16,7 @@ gas_gravity = float(gas_gravity)
 
 # the if statement
 
-if co2_comp > 0.12 or n2_comp > 0.03 or h2s_comp > 0:
+if co2_comp <= 0.12 or n2_comp <= 0.03 or h2s_comp <= 0:
     gas_gravity = (gas_gravity - (1.1767*h2s_comp) - \
                       (1.5196*co2_comp) - (0.9672*n2_comp) - \
                        (0.622*h2o_comp))/(1- h2s_comp - co2_comp - n2_comp - h2o_comp)
@@ -29,8 +29,12 @@ if co2_comp > 0.12 or n2_comp > 0.03 or h2s_comp > 0:
 
 p_pch = 756.8 - (131*gas_gravity) - (3.6*gas_gravity**2)
 t_pch = 169.2 + (349.5*gas_gravity) - (74.0*gas_gravity**2)
+ppc=(1-h2s_comp-co2_comp-n2_comp-h2o_comp)*p_pch+1306*h2s_comp+1071*co2_comp+493.1*n2_comp+3200.1*h2o_comp
+tpc=(1-h2s_comp-co2_comp-n2_comp-h2o_comp)*t_tch+672.35*h2s_comp+547.58*co2_comp+227.16*n2_comp+1164.9*h2o_comp
 
 # displaying the results.
 print('The hydrocarbon pseudo-critical pressure is {0:.2f} psia'.format(p_pch))
 print('The hydrocarbon pseudo-critical temperature is {0:.2f} deg Rankine'.format(p_pch))
+print('the pseudo reduced pressure is ',ppc) 
+print('the pseudo reduced temperature is', tpc)
  
